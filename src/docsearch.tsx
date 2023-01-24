e1b49ed19c1f9cf27bb2ad434da2338740500ccb
Achille Lacoin <achille.lacoin@gmail.com>
2023-01-24 17:42:34 +0800

    fix: fix searchbar text disappearing
    * fix searchbar text disappearing
    * fix typo in changelog
    * fix duplicated key warning
    * add icon for default action in docsearch

--- src/docsearch.tsx
+++ src/docsearch.tsx
@@ -2,7 +2,7 @@ import APIData from "./algolia/apiData";
 import type { IAPIData } from "./algolia/types";
 import { SearchDocumentation } from "./components";
 
-import { ActionPanel, List, Action, useNavigation } from "@raycast/api";
+import { ActionPanel, List, Action, useNavigation, Icon } from "@raycast/api";
 import { useEffect, useState } from "react";
 
 export default function ChooseSearchDocumentation() {
@@ -27,13 +27,14 @@ export default function ChooseSearchDocumentation() {
       {currentAPIData?.map((API: IAPIData) => (
         <List.Item
           icon={API.icon}
-          key={API.apiKey}
+          key={`${API.homepage}-${API.indexName}`}
           title={API.name}
           subtitle={API.lang}
           actions={
             <ActionPanel>
               <Action
                 title="Choose"
+                icon={Icon.ArrowRight}
                 onAction={() => {
                   push(<SearchDocumentation docsName={API.name} lang={API.lang} />);
                 }}
